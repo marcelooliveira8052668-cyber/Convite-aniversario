00f92556656c50f2c55686f6d4199aff682c0742
Prof. Marcelo Oliveira <marcelo.oliveira8052668@gmail.com>
2026-04-25 16:07:28 -0300

    Change WhatsApp URL to SurveyMars link

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function confirmar() {
     const numero = "5511969911236";
     const mensagem = "Oi Sylvia! Claro que eu estarei ai no seu aniversário😊🎉";
 
-    const url = "https://api.whatsapp.com/send?phone=" + numero + "&text=" + encodeURIComponent(mensagem);
+    const url = "https://surveymars.com/q/NGN1OVBi4" + numero + "&text=" + encodeURIComponent(mensagem);
 
     window.location.href = url;
 }
